db5e667c9b0ed8fc5b53b7dc44a3d520645e7677
stefan <stefan@sigrow.com>
2024-11-27 20:39:52 +0100

    Adding sntp feature

--- main/tasks_common.h
+++ main/tasks_common.h
@@ -7,7 +7,6 @@
 
 #ifndef MAIN_TASKS_COMMON_H_
 #define MAIN_TASKS_COMMON_H_
-
 // WiFi application task
 #define WIFI_APP_TASK_STACK_SIZE			4096
 #define WIFI_APP_TASK_PRIORITY				5
@@ -23,9 +22,19 @@
 #define HTTP_SERVER_MONITOR_PRIORITY		3
 #define HTTP_SERVER_MONITOR_CORE_ID			0
 
-// HTTP Server Monitor task
+// Wifi Reset Button task
+#define WIFI_RESET_BUTTON_TASK_STACK_SIZE	2048
+#define WIFI_RESET_BUTTON_TASK_PRIORITY		6
+#define WIFI_RESET_BUTTON_TASK_CORE_ID		0
+
+// DHT22 Sensor task
 #define DHT22_TASK_STACK_SIZE				4096
 #define DHT22_TASK_PRIORITY					5
-#define DHT22_TASK_CORE_ID					0
+#define DHT22_TASK_CORE_ID					1
+
+// SNTP Time Sync task
+#define SNTP_TIME_SYNC_TASK_STACK_SIZE		4096
+#define SNTP_TIME_SYNC_TASK_PRIORITY		4
+#define SNTP_TIME_SYNC_TASK_CORE_ID			1
 
 #endif /* MAIN_TASKS_COMMON_H_ */
